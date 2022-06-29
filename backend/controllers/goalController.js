1651764723f3a9bfb843b43953751432258f44d4
Tiago Gomes <tidgomes@gmail.com>
2022-06-29 16:34:11 -0300

    controller created and connected to the router

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,22 @@
+const getGoals = (req, res) => {
+    res.status(200).json({ message: 'Get goals' })
+}
+
+const setGoal = (req, res) => {
+    res.status(200).json({ message: 'Goal set' })
+}
+
+const updateGoal = (req, res) => {
+    res.status(200).json({ message: `Goal ${req.params.id} updated` })
+}
+
+const deleteGoal = (req, res) => {
+    res.status(200).json({ message: `Goal ${req.params.id} deleted` })
+}
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+}
